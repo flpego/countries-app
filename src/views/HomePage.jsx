5f047506260669da31f1ed8805549db9b7f01b47
flpego <flaviopegoraro2@gmail.com>
2025-04-03 11:53:29 -0300

    cambios en homePage

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,20 +1,9 @@
-
-import { useContext, useEffect } from "react";
 import CountriesList from "../components/Countries/CountriesList"
 import Header from "../components/Header/Header"
 import FormContainer from "../components/SearchForm/FormContainer"
-import { CountriesContext } from "../contexts/CountriesContext";
-import { useLocation } from "react-router-dom";
 import "../styles/HomePage.css"
 
 const HomePage = () => {
-
-  const { loadCountries } = useContext(CountriesContext);
-  const location = useLocation();
-
-  useEffect(() => {
-    loadCountries(); // Recargar países cada vez que se accede a Home
-  }, [loadCountries, location.pathname]);
 
   return (
     <div>
